b341c23ab9af00bf4fc856d318bb143917fb3e56
mohamed mahmoud <mshaat@10degrees.io>
2019-02-09 22:54:43 +0200

    Initial Commit

---
+++ Camera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Camera
+//
+//  Created by LapStore on 2/9/19.
+//  Copyright © 2019 shaat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
